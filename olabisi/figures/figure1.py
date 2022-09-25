1e52833ab1a88864d33aac4b42c98ae28fbabf11
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2022-09-25 16:01:30 -0700

    Investigating raw cytokines levels of each location and treatment

--- olabisi/figures/figure1.py
+++ olabisi/figures/figure1.py
@@ -1,6 +1,7 @@
 """
-Figure 1
+Figure 1: Investigating of tensor decomposition of mean cytokine signals
 """
+from unittest import skip
 import numpy as np
 import seaborn as sns
 from .common import subplotLabel, getSetup
@@ -12,26 +13,23 @@ from ..plsr import plsr_model
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((13, 16), (3, 3))
+    ax, f = getSetup((13, 16), (2, 3))
 
     # Add subplot labels
     subplotLabel(ax)
     
-    olabisiXA, olabisiDF, meanDF = import_olabisi_hemi_xa(lod=True, perc_per_cyt=0.1, data="nomgh1")
-    plsr_model(olabisiDF,ax[0],ax[1],ax[2], ncomp=10)
-    plsr_model(meanDF,ax[3],ax[4],ax[5], ncomp=10)
-    
-    # print(olabisiXA)
-    # for i, days in enumerate(olabisiXA["Day"].values):
-    #     print("Day:", days, "Missing Values Percentange:", np.mean(np.isnan(olabisiXA.isel(Day=i).values)))
-    # print("Total Missing Values Percentange:", np.mean(np.isnan(olabisiXA.to_numpy())))  
+    olabisiXA, olabisiDF, meanDF = import_olabisi_hemi_xa(lod=False, perc_per_cyt=0.1, data="nomgh1")
+                    
+    for i, days in enumerate(olabisiXA["Day"].values):
+        print("Day:", days, "Missing Values Percentange:", np.mean(np.isnan(olabisiXA.isel(Day=i).values)))
+    print("Total Missing Values Percentange:", np.mean(np.isnan(olabisiXA.to_numpy())))  
 
-    # R2Xplot(ax[0], olabisiXA, rank=6)
-    # fac_df = tFac_DF(X=olabisiXA, rank=6)
+    R2Xplot(ax[0], olabisiXA, rank=6)
+    fac_df = tFac_DF(X=olabisiXA, rank=6)
 
-    # for i in range(0, 4):
-    #     """Plots tensor factorization of cells"""
-    #     sns.heatmap(data=fac_df[i], ax=ax[i + 1],
-    #         cmap=sns.diverging_palette(240, 10, as_cmap=True),)
+    for i in range(0, 4):
+        """Plots tensor factorization of cells"""
+        sns.heatmap(data=fac_df[i], ax=ax[i + 1],
+            cmap=sns.diverging_palette(240, 10, as_cmap=True),)
 
     return f
